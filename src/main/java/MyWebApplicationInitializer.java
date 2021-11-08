fa6ebb468c4716126bc7b34cf77a0ed2677bed07
Darina <37955869+dagenator@users.noreply.github.com>
2021-11-08 14:47:55 +0500

    Dispatcher Servlet added

---
+++ src/main/java/MyWebApplicationInitializer.java
@@ -0,0 +1,15 @@
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet());
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/list/*");
+    }
+
+}
